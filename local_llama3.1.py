d8b1180b3f446413f91ca8d67d12279bb50fb449
Sanjay Amirthraj <70417048+sanjayamirthraj@users.noreply.github.com>
2024-07-24 15:28:27 -0700

    Update local_llama3.1.py

--- local_llama3.1.py
+++ local_llama3.1.py
@@ -5,23 +5,28 @@ st.title("Local Chat App")
 st.caption("This app allows you to chat with an AI using local llama3 and RAG all locally run on your machine!")
 st.caption("For any questions, contact Sanjay Amirthraj")
 
-# Prompt the user for their message or question and context
 context = st.text_area("Enter the context for your question", height=300)
 prompt = st.text_input("Enter your message or question")
 
-if prompt and context:
-    # Call Ollama Llama3 model
+if prompt:
     def ollama_llm(question, context):
+        if not context ==  "":
+            context = "Help the user to the most of your abilities"
         formatted_prompt = f"Question: {question}\n\nContext: {context}"
         response = ollama.chat(model='llama3.1', messages=[{'role': 'user', 'content': formatted_prompt}])
         return response['message']['content']
 
-    # Chat with the AI
     result = ollama_llm(prompt, context)
     st.write(result)
 
-# Add a button to clear the input
 
 if st.button("Submit"):
     prompt = ""
     context = ""
+
+import datetime
+st.write("Chat time: ", datetime.datetime.now())
+
+
+
+
